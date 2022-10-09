13f69e097c3ed8d0c861e8e28ca06296f51da10f
jaguar017 <jayesh.singh2019@vitstudent.ac.in>
2022-10-09 18:18:46 +0530

    warning removed

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {useRef} from 'react'
+//import {useRef} from 'react'
 import { useGLTF} from '@react-three/drei'
 function Model({ ...props }) {
-    const group = useRef()
+    //const group = useRef()
     const { nodes, materials } = useGLTF('../../nftverse.glb')
     return (
       <group {...props} dispose={null} scale={0.2}>
